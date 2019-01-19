ba07b53cbccef5ab744148ce31809987c3d17af2
jonathan <jonathancodesaway@gmail.com>
2019-01-19 08:31:24 -0800

    quizlist and quizans set up, using ranges to query, better scalability

--- postgrestest/db.rb
+++ postgrestest/db.rb
@@ -17,15 +17,33 @@ questions json
 ex: {"do you like chicken?" : {"yes" : 1, "no" : 0, "maybe" : -1}} 
 the question is a key and the value contains another json with all answers being the keys and the values being the weights of the answers
 the final result is the culmination of all the weights after all questions have been answered. 
-
-results json
-ex: {"-3, -1" : "Jasmine", "0, 1" : "Bella", "2, 3" : "Cinderalla"}
-I'm not a fan of this. We take a range of values and check if the final result is within the ranges, inclusive, when it finds the range
-it is in, return the value. I feel like this is slow. Look into int4range and arrays in postgres. The problem I see in the int4range or arrayis that we might need to create a seperate table of quiz answers, which means the amount of total possible answers will be predefined. I don't like that for long term, but then again, this is a capstone.
 =end
 
 def create_quizlist_table()
-  $conn.exec("create table if not exists quizlist(id int primary key not null, title text not null, questions json not null, results json not null)")
+  #create basic quizlist table
+  $conn.exec("create table if not exists quizlist(id int primary key not null, title text not null, questions json not null)")
+  #create first quiz answer table
+  $conn.exec(createQuizAns(1))
+  #prepare statements, inserquiz is to insert a new quiz into quizlist. insertans is to insert new answers into quizans#{id}
+  $conn.prepare('insertquiz', 'insert into quizlist (id, title, questions) values ($1, $2, $3)')
+  $conn.prepare('insertans', insertQuizAns(1))
+  
+  #create the first quiz
+  $conn.exec_prepared('insertquiz', [1,'Which disney princess are you?', '{"Do you like to fight?": {"yes" : 1, "no" : 0 }}'])
+  $conn.exec_prepared('insertans', [[1,1], "Mulan"])
+  $conn.exec_prepared('insertans', [[0,0], "Bella"])
 end 
+
+def insertQuizAns(id)
+  temp = "insert into quizans#{id} (search, result) values ($1, $2)"
+  puts temp
+  return temp
+end
+
+def createQuizAns(id)
+  temp = "create table if not exists quizans#{id}(search int4range, result text)"
+  puts temp
+  return temp
+end
 
 db_use("quizapp")
